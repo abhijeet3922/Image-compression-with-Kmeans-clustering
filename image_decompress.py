7e1cafff8bb502d179fdf2e39b5eeb8e5b22d85b
Abhijeet Kumar <abhijeet.charlie@gmail.com>
2017-03-08 14:37:07 +0530

    Add files via upload

---
+++ image_decompress.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 23:18:12 2017
+
+@author: user
+"""
+from skimage import io
+import numpy as np
+
+centers = np.load('codebook_tiger.npy')
+
+c_image = io.imread('compressed_tiger.png')
+
+image = np.zeros((c_image.shape[0],c_image.shape[1],3),dtype=np.uint8 )
+for i in range(c_image.shape[0]):
+    for j in range(c_image.shape[1]):
+            image[i,j,:] = centers[c_image[i,j],:]
+    
+io.imsave('reconstructed_tiger.png',image);
+io.imshow(image)
+io.show()
---
+++ img_compress.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from skimage import io
+from sklearn.cluster import KMeans
+import numpy as np
+
+image = io.imread('C:\\Users\\Charlie\\Desktop\\image-compression\\tiger.png')
+io.imshow(image)
+io.show()
+
+rows = image.shape[0]
+cols = image.shape[1]
+ 
+image = image.reshape(image.shape[0]*image.shape[1],3)
+kmeans = KMeans(n_clusters = 128, n_init=10, max_iter=200)
+kmeans.fit(image)
+
+clusters = np.asarray(kmeans.cluster_centers_,dtype=np.uint8) 
+labels = np.asarray(kmeans.labels_,dtype=np.uint8 )  
+labels = labels.reshape(rows,cols); 
+
+np.save('codebook_tiger.npy',clusters)    
+io.imsave('compressed_tiger.png',labels);
+
+
+
