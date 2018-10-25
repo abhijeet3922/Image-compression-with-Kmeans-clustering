1117d56fb1c2a3e2b7f66df49d7ee4b9875087dc
Sajjad Ayobi <43270899+sajjjadayobi@users.noreply.github.com>
2018-10-25 12:20:36 -0700

    a little easier

--- image_decompress.py
+++ image_decompress.py
@@ -4,14 +4,9 @@ from skimage import io
 import numpy as np
 
 centers = np.load('codebook_tiger.npy')
-
 c_image = io.imread('compressed_tiger.png')
+image = centers[c_image]
 
-image = np.zeros((c_image.shape[0],c_image.shape[1],3),dtype=np.uint8 )
-for i in range(c_image.shape[0]):
-    for j in range(c_image.shape[1]):
-            image[i,j,:] = centers[c_image[i,j],:]
-    
 io.imsave('reconstructed_tiger.png',image);
 io.imshow(image)
 io.show()
